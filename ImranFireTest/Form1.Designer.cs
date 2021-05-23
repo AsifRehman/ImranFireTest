9d5e018ff095c984c0f1306674c3dd3486172c86
Asif <asif.pro@gmail.com>
2021-05-23 19:05:41 +0500

    Finished The Product

--- ImranFireTest/Form1.Designer.cs
+++ ImranFireTest/Form1.Designer.cs
@@ -30,17 +30,24 @@ namespace ImranFireTest
         private void InitializeComponent()
         {
             this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
             this.button1 = new System.Windows.Forms.Button();
             this.button2 = new System.Windows.Forms.Button();
             this.lblStat = new System.Windows.Forms.Label();
             this.timer1 = new System.Windows.Forms.Timer(this.components);
             this.btnStartTimer = new System.Windows.Forms.Button();
             this.btnStopTimer = new System.Windows.Forms.Button();
+            this.colorDialog1 = new System.Windows.Forms.ColorDialog();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.label1 = new System.Windows.Forms.Label();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.panel1.SuspendLayout();
+            this.panel2.SuspendLayout();
             this.SuspendLayout();
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(70, 157);
+            this.button1.Location = new System.Drawing.Point(69, 195);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(109, 23);
             this.button1.TabIndex = 0;
@@ -51,18 +58,19 @@ namespace ImranFireTest
             // 
             // button2
             // 
-            this.button2.Location = new System.Drawing.Point(70, 72);
+            this.button2.BackColor = System.Drawing.SystemColors.GradientInactiveCaption;
+            this.button2.Location = new System.Drawing.Point(69, 156);
             this.button2.Name = "button2";
             this.button2.Size = new System.Drawing.Size(215, 23);
             this.button2.TabIndex = 1;
             this.button2.Text = "Manual Sync To Cloud";
-            this.button2.UseVisualStyleBackColor = true;
+            this.button2.UseVisualStyleBackColor = false;
             this.button2.Click += new System.EventHandler(this.button2_Click);
             // 
             // lblStat
             // 
             this.lblStat.AutoSize = true;
-            this.lblStat.Location = new System.Drawing.Point(19, 310);
+            this.lblStat.Location = new System.Drawing.Point(41, 5);
             this.lblStat.Name = "lblStat";
             this.lblStat.Size = new System.Drawing.Size(0, 13);
             this.lblStat.TabIndex = 2;
@@ -74,40 +82,80 @@ namespace ImranFireTest
             // 
             // btnStartTimer
             // 
-            this.btnStartTimer.Location = new System.Drawing.Point(291, 72);
+            this.btnStartTimer.BackColor = System.Drawing.SystemColors.GradientInactiveCaption;
+            this.btnStartTimer.Location = new System.Drawing.Point(290, 156);
             this.btnStartTimer.Name = "btnStartTimer";
             this.btnStartTimer.Size = new System.Drawing.Size(194, 23);
             this.btnStartTimer.TabIndex = 3;
             this.btnStartTimer.Text = "Start Auto Sync";
-            this.btnStartTimer.UseVisualStyleBackColor = true;
+            this.btnStartTimer.UseVisualStyleBackColor = false;
             this.btnStartTimer.Click += new System.EventHandler(this.btnStartTimer_Click);
             // 
             // btnStopTimer
             // 
+            this.btnStopTimer.BackColor = System.Drawing.SystemColors.GradientInactiveCaption;
             this.btnStopTimer.Enabled = false;
-            this.btnStopTimer.Location = new System.Drawing.Point(491, 72);
+            this.btnStopTimer.Location = new System.Drawing.Point(490, 156);
             this.btnStopTimer.Name = "btnStopTimer";
             this.btnStopTimer.Size = new System.Drawing.Size(194, 23);
             this.btnStopTimer.TabIndex = 3;
             this.btnStopTimer.Text = "Stop Auto Sync";
-            this.btnStopTimer.UseVisualStyleBackColor = true;
+            this.btnStopTimer.UseVisualStyleBackColor = false;
             this.btnStopTimer.Click += new System.EventHandler(this.btnStopTimer_Click);
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.SystemColors.HotTrack;
+            this.panel1.Controls.Add(this.label1);
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Top;
+            this.panel1.Location = new System.Drawing.Point(0, 0);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(777, 83);
+            this.panel1.TabIndex = 4;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Tahoma", 30F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)), true);
+            this.label1.ForeColor = System.Drawing.SystemColors.ButtonFace;
+            this.label1.Location = new System.Drawing.Point(61, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(596, 48);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Cloud Sync Management System";
+            // 
+            // panel2
+            // 
+            this.panel2.BackColor = System.Drawing.SystemColors.Highlight;
+            this.panel2.Controls.Add(this.lblStat);
+            this.panel2.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.panel2.Location = new System.Drawing.Point(0, 415);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(777, 35);
+            this.panel2.TabIndex = 5;
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.BackgroundImage = global::ImranFireTest.Properties.Resources.background;
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.ClientSize = new System.Drawing.Size(777, 450);
+            this.Controls.Add(this.panel2);
+            this.Controls.Add(this.panel1);
             this.Controls.Add(this.btnStopTimer);
             this.Controls.Add(this.btnStartTimer);
-            this.Controls.Add(this.lblStat);
             this.Controls.Add(this.button2);
             this.Controls.Add(this.button1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
             this.Name = "Form1";
-            this.Text = "Form1";
+            this.Text = "Sync Form";
             this.Load += new System.EventHandler(this.Form1_Load);
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.panel2.ResumeLayout(false);
+            this.panel2.PerformLayout();
             this.ResumeLayout(false);
-            this.PerformLayout();
 
         }
 
@@ -119,6 +167,10 @@ namespace ImranFireTest
         private System.Windows.Forms.Timer timer1;
         private System.Windows.Forms.Button btnStartTimer;
         private System.Windows.Forms.Button btnStopTimer;
+        private System.Windows.Forms.ColorDialog colorDialog1;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Panel panel2;
     }
 }
 
